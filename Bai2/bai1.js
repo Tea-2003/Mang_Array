dc0756381a8b705e739e95d29de1089f04ada797
unknown <admin@pnvd244.pn.com.vn>
2023-02-07 16:35:34 +0700

    push

---
+++ Bai2/bai1.js
@@ -0,0 +1,7 @@
+
+var fruits = ["banana", "Orange", "Apple", "Kiwi"];
+document.getElementById("demo").innerHTML = fruits;
+function myFunction() {
+    fruits.splice(2, 0, "Lemon", "Mango");
+    document.getElementById("demo").innerHTML = fruits;
+}
